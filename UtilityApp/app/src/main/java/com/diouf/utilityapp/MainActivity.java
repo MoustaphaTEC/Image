0e5008df1dad668feb1ed46b62daea9f8aaf8efc
MoustaphaTEC <dioufawwalou@gmail.com>
2019-12-03 01:07:19 +0000

    partie 6

---
+++ UtilityApp/app/src/main/java/com/diouf/utilityapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.diouf.utilityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
