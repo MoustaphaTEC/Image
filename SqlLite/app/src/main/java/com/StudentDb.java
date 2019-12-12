7a479b8ac815f1fd6c3645c975ceabb1dd3664e2
MoustaphaTEC <dioufawwalou@gmail.com>
2019-12-12 23:38:55 +0000

    tp_en_classe

--- SqlLite/app/src/main/java/com/StudentDb.java
+++ SqlLite/app/src/main/java/com/StudentDb.java
@@ -1,6 +1,31 @@
 package com;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class StudentDb extends SQLiteOpenHelper {
+
+    private static  String DBname = "student1.db";
+    private static int DBversion = 1;
+
+    public  StudentDb(Context context){
+
+        super(context, DBname, null, DBversion);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE student(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void onOpen(SQLiteDatabase db){
+        super.onOpen(db);
+    }
 }
