10d38e1931c6cfaced6ad492fc5e223425aaebea
MoustaphaTEC <dioufawwalou@gmail.com>
2019-12-12 23:35:22 +0000

    tp_en_classe

---
+++ FragApp/app/src/main/java/com/helps/fragapp/ui/main/MainFragment.java
@@ -0,0 +1,58 @@
+package com.helps.fragapp.ui.main;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.helps.fragapp.R;
+
+public class MainFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.main_fragment, container, false);
+        Button btnClick = (Button) getActivity().findViewById(R.id.clik);
+        if (btnClick == null){
+            Log.i("MAINFRAGMENT", "btnClick null");
+        }
+        else {
+            Log.i("MAINFRAGMENT", "btnClick not null");
+        }
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Clicked", Toast.LENGTH_SHORT).show();
+                Log.i("MAINFRAGMENT", "clicked");
+            }
+        });
+        return  v;
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
